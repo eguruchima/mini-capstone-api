3e0ad7ff0f993f5ddd5f23d2803d37b4993aa087
eguruchima <eguruchima@gmail.com>
2025-02-26 15:45:35 -0500

    Progress

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,10 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :description, length: { in: 1..500 }
 
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end
+
   def is_discounted?
     price && price <= 10
   end

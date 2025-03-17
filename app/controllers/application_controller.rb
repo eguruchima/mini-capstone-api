83277e44ccac4b83d82f1deea78843044288ec6e
eguruchima <eguruchima@gmail.com>
2025-03-17 13:30:27 -0400

    Added user authentication/cookie verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    User.find_by(id: cookies.signed[:user_id])
+  end
+
+  def authenticate_user
+    unless current_user
+      render json: {}, status: :unauthorized
+    end
+  end
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
   allow_browser versions: :modern

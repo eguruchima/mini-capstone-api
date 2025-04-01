140a61f7d5f39a68dd515acffbf33c9190786b28
eguruchima <eguruchima@gmail.com>
2025-04-01 14:16:30 -0400

    added more complex relationships to product and user using table carted products as connecting table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
+  has_many :orders, through: :carted_products
 
 
   validates :name, presence: true

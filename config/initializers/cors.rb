d3187cb63ced0ccf2cf1f67713144694a74562ca
eguruchima <eguruchima@gmail.com>
2025-04-01 14:07:40 -0400

    added models carted products and migration to remove product id

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,17 @@
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins "mini-capstone.peterxjang.com", "localhost:517"
+#     resource "*", headers: :any, credentials: true, methods: [ :get, :post, :patch, :put, :delete ]
+#   end
+# end
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "mini-capstone.peterxjang.com", "localhost:517"
-    resource "*", headers: :any, credentials: true, methods: [ :get, :post, :patch, :put, :delete ]
+    origins "http://mini-capstone.peterxjang.com", "http://localhost:5173"
+
+    resource "*",
+      headers: :any,
+      credentials: true,
+      methods: [ :get, :post, :patch, :put, :delete, :options ]
   end
 end
